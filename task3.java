449459b61546d37a850710d7f45e212c73b609b5
Saglara <nohaevass@yandex.ru>
2023-02-03 21:31:04 +0300

    Update task3.java

--- task3.java
+++ task3.java
@@ -20,6 +20,51 @@
 // ответ:
 // 9
 
-public class task3 {
-    
+import java.util.Scanner;
+import java.util.Stack;
+
+public class task3 {    
+    public static void main(String[] args) {
+
+        Scanner iScanner = new Scanner(System.in);
+        Stack <Float> stack = new Stack<>();
+
+        System.out.printf("Введите первое число = ");
+        float number1 = iScanner.nextInt();
+
+        System.out.printf("Введите второе число = ");
+        float number2 = iScanner.nextInt();
+
+        System.out.printf("Введите действие (+, -, * или /): ");
+        Character act = iScanner.next().charAt(0);
+        Float rezult=(float) 0;
+
+        if (act == '+') {
+            rezult = number1 + number2;
+            System.out.printf("%f + %f = %f", number1, number2, rezult);
+        }
+
+        if (act == '-') {
+            rezult = number1 - number2;
+            System.out.printf("%f - %f = %f", number1, number2, rezult);
+        }
+        if (act == '*') {
+            rezult = number1 * number2;
+            System.out.printf("%f * %f = %f", number1, number2, rezult);
+        }
+        if (act == '/') {
+            rezult = number1 / number2;
+            System.out.printf("%f : %f = %f", number1, number2, rezult);
+        }
+        
+        
+        stack.push(rezult);
+
+
+
+
+
+        iScanner.close();
+
+    }
 }
